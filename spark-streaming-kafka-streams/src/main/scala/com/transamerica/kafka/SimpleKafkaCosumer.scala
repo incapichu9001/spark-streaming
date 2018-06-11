1bb36eecc81f57c8c62f5699e2b83cdd478a43ba
mtarigopula <mtarigopula@USPL3YBZXY1.us.aegon.com>
2018-06-11 16:50:23 -0500

    First Commit to local repo

---
+++ spark-streaming-kafka-streams/src/main/scala/com/transamerica/kafka/SimpleKafkaCosumer.scala
@@ -0,0 +1,16 @@
+package com.transamerica.kafka
+
+import java.util.Properties
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import scala.collection.JavaConverters._
+
+object SimpleKafkaCosumer extends App{
+
+  val topic = "Test"
+  
+  val props:Properties = new Properties()
+  
+  val x = props.put("key", "value")
+  
+  
+}
---
+++ spark-streaming-kafka-streams/src/main/scala/com/transamerica/kafka/SimpleKafkaProducer.scala
@@ -0,0 +1,33 @@
+package com.transamerica.kafka
+
+import java.util.Properties
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, RecordMetadata}
+import java.util.concurrent.Future
+
+
+object SimpleKafkaProducer extends App{
+  
+  val TOPIC = "KafkaTopic1"
+  val props = new Properties()
+  props.put("bootstrap.servers", "localhost:9092")
+  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+ 
+  val producer = new KafkaProducer[String, String](props)
+  try{
+    for(i<- 1 to 100) {
+      
+      val record = new ProducerRecord(TOPIC, s"key-$i", s"hello $i")
+      val result: Future[RecordMetadata] = producer.send(record)
+      
+    }
+  }catch {
+    case e:Exception => e.printStackTrace()
+    producer.close()
+  }
+  finally {
+    producer.close()
+  }
+  
+  
+}
